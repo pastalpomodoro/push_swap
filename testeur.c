5a20d2db5f1d47e1ff115d686591b2ee0d7c2fb0
thomas <thomas@debiantoto.home>
2024-12-06 15:55:06 +0100

    premiere reussite

--- testeur.c
+++ testeur.c
@@ -24,7 +24,21 @@ void print_b(t_num **pile_b)
     }
     ft_printf("\n");
 }
+// fonction qui permet de comprendre sy le premier numero de la pile est le plus petit de toute la pile
+int if_smaller(t_num *pile)
+{
+    int n;
 
+    n = pile->num;
+    while (pile != NULL)
+    {
+        if (n > pile->num)
+            return (0);
+        pile = pile->next;
+    }
+    return (1);
+}
+// fonction qui permet de comprendre sy le premer numero de la pile est plus grand que l enumero d apres
 int if_bigger(t_num *pile)
 {
     t_num *temp;
@@ -40,14 +54,23 @@ int if_bigger(t_num *pile)
 
 void mooving(t_num **pile_a, t_num **pile_b)
 {
+    int count;
+
+    count = 0;
     while (my_lstlen(pile_a) != 0)
     {
-        if (if_bigger(*pile_a) == 1)
-            sa(pile_a);
-        pb(pile_a, pile_b);
-        if (if_bigger(*pile_b) == 0)
-            sb(pile_b);
+        if (if_smaller(*pile_a) == 1)
+        {
+            pb(pile_a, pile_b);
+            count++;
+        }
+        else
+        {
+            ra(pile_a);
+            count++;
+        }
     }
+    ft_printf("%d\n", count);
 }
 
 void test(t_num **pile_a, t_num **pile_b)
